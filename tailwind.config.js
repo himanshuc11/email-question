d812eb3c087d210cbb9730e9a4d89176745119a2
himanshuc11 <himanshuchhatpar@gmail.com>
2023-10-22 16:47:48 +0530

    style: Added colors scheme for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  theme: {
+    colors: {
+      primary: "#E54065",
+      secondary: "#F4F5F9",
+      borderGrey: "#CFD2DC",
+      textGrey: "#636363",
+      filterGrey: "#E1E4EA",
+      readWhite: "#F2F2F2"
+    }
+  },
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
